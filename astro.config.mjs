d3896fe0fc600323348eb1ad7e158afc7e98565d
edl-tech <emiliodl2007@gmail.com>
2025-05-30 22:44:39 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://edl-tech.github.io',
+  base: '/aevum-agora/',
 });
